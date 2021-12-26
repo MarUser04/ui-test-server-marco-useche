f6b7815e60427d0db9de23a172af36f1d6fbe06e
Marco Useche <usechermarcoj@gmail.com>
2021-12-25 22:15:53 -0400

    add new start script

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import regeneratorRuntime from "regenerator-runtime";
 import cors from "cors";
 import "dotenv/config";
 import morgan from "morgan";
--- src/routes/controversial.routes.js
+++ src/routes/controversial.routes.js
@@ -4,6 +4,7 @@ import { getData, vote } from "../controllers/controversial.controller";
 const router = Router();
 
 router.get("/", getData);
+
 router.post("/vote/:id", vote);
 
 export default router;
